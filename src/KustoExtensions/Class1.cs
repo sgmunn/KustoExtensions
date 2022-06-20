9071ac8f8b741539866b2a2dad385b794a8f2ed2
Greg Munn <gregm@microsoft.com>
2022-06-20 11:33:53 -0400

    Initial attempt to create an extension

---
+++ src/KustoExtensions/Class1.cs
@@ -0,0 +1,16 @@
+﻿using MonoDevelop.Components.Commands;
+
+namespace KustoExtensions
+{
+    class InitializeCommand : CommandHandler
+    {
+        protected override void Run()
+        {
+            //IdeApp.Initialized += (s, e) =>
+            //{
+            //    var propertyPanelDelegate = new PropertyPanelDelegate();
+            //    Runtime.Initialize(propertyPanelDelegate);
+            //};
+        }
+    }
+}
---
+++ src/KustoExtensions/Properties/AddinInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.Versioning;
+using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly: Addin(
+    "KustoExtensions",
+    Namespace = "VisualStudio",
+    Version = "0.0.1"
+)]
+
+[assembly: AddinName("VisualStudio Kusto Extensions")]
+[assembly: AddinCategory("IDE extensions")]
+[assembly: AddinDescription("Run your Kusto Queries from the IDE")]
+[assembly: AddinAuthor("Greg Munn")]
