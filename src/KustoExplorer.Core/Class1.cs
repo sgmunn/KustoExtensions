345ec741118a01a06008aefc1b2b662e1bef3e1e
Greg Munn <gregm@microsoft.com>
2022-06-21 10:28:22 -0400

    Try a standalone project and a shared library

---
+++ src/KustoExplorer.Core/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Kusto.Data.Net.Client;
+
+namespace KustoExplorer.Core
+{
+    public class Class1
+    {
+
+        public string GetTest(string query)
+        {
+            // todo: we need to be able to define a list of these
+            var client = KustoClientFactory.CreateCslQueryProvider("https://ddtelinsights.kusto.windows.net/DDTelInsights;Fed=true");
+
+            //var reader = client.ExecuteQuery(".show function VSMTI_VSMac_PublicVersionsWithFirstReleaseDate");
+            var reader = client.ExecuteQuery(query);
+            if (reader.RecordsAffected == 0)
+            {
+
+            }
+
+
+            return "test";
+        }
+    }
+}
+
---
+++ src/KustoExplorer/ViewController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using AppKit;
+using Foundation;
+using KustoExplorer.Core;
+using ObjCRuntime;
+
+namespace KustoExplorer
+{
+	public partial class ViewController : NSViewController
+	{
+		public ViewController (NativeHandle handle) : base (handle)
+		{
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+
+			// Do any additional setup after loading the view.
+
+			var x = new Class1();
+			x.GetTest("print now()");
+		}
+
+		public override NSObject RepresentedObject {
+			get {
+				return base.RepresentedObject;
+			}
+			set {
+				base.RepresentedObject = value;
+				// Update the view, if already loaded.
+			}
+		}
+	}
+}
