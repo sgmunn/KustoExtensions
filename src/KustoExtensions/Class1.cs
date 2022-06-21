34d6f339a39bf55172777ccadff487c6fc0a04ba
Greg Munn <gregm@microsoft.com>
2022-06-21 15:15:51 -0400

    ok, so finally got something cobbled together that actually loads

--- src/KustoExtensions/Class1.cs
+++ src/KustoExtensions/Class1.cs
@@ -2,15 +2,15 @@
 
 namespace KustoExtensions
 {
-    class InitializeCommand : CommandHandler
-    {
-        protected override void Run()
-        {
-            //IdeApp.Initialized += (s, e) =>
-            //{
-            //    var propertyPanelDelegate = new PropertyPanelDelegate();
-            //    Runtime.Initialize(propertyPanelDelegate);
-            //};
-        }
-    }
+    //class InitializeCommand : CommandHandler
+    //{
+    //    protected override void Run()
+    //    {
+    //        //IdeApp.Initialized += (s, e) =>
+    //        //{
+    //        //    var propertyPanelDelegate = new PropertyPanelDelegate();
+    //        //    Runtime.Initialize(propertyPanelDelegate);
+    //        //};
+    //    }
+    //}
 }
